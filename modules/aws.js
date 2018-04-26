2e6a7ed75759196ce54e137885db41a0f778a1ec
Guilherme Oderdenge <guilhermeoderdenge@gmail.com>
2018-04-26 17:59:33 -0300

    Implement support for GameLiftLocal

--- modules/aws.js
+++ modules/aws.js
@@ -1,15 +1,18 @@
-// ====================================================================================================
-//
-// Cloud Code for AWS, write your code here to customize the GameSparks platform.
-//
-// For details of the GameSparks Cloud Code API see https://docs.gamesparks.com/
-//
-// ====================================================================================================
-
-// Version 1.0.0
+/**
+ * @module sparkslift
+ * @description Easily integrate GameSparks with GameLift.
+ * @version 1.1.0
+ */
 
 require('sha')
 require('GameLiftCredentials')
+
+// This library will always perform actions only against GameLift.
+var SERVICE = 'gamelift'
+
+// The default request method the actions will be performed.
+// GameLift uses 'POST' as the default one for every and each action.
+var DEFAULT_REQUEST_METHOD = 'POST'
 
 function hmac (key, keyEncoding, string) {
   var shaObj = new JSSHA('SHA-256', 'TEXT')
@@ -24,39 +27,47 @@ function hash (string) {
 }
 
 /**
- * AWS
- * @param - options.region - Region of this AWS Service
- * @param - options.action - The action to request. Ie. CreateGameSession, SearchGameSessions.
- * @param - options.headers - Request headers given in object format {"key": "value"}
- * @param - options.params - Request parameters given in object format {"key": "value"}
- * @param - options.payload - Request payload given in object format {"key": "value"}
+ * The AWS object
+ * @param {Object} options All the options needed to perform AWS requests.
+ * @param {string} options.region Region of the fleet corresponding to GameLift.
+ * @param {string} options.action The action to perform the request. Ie. CreateGameSession, SearchGameSessions.
+ * @param {string[]} [options.headers] Request headers given in object format {"key": "value"}
+ * @param {Object[]} [options.params] Request parameters given in object format {"key": "value"}
+ * @param {Object} [options.payload] Request payload given in object format {"key": "value"}
+ * @param {Object} [options.local] Information needed to perform requests to GameLiftLocal
+ * @param {string} options.local.address The address (IP, domain, etc.) where GameLiftLocal is running at.
+ * @param {number} options.local.port The port GameLiftLocal is listening to.
+ * @returns {Object} response The response object of the performed request.
  **/
-
 var AWS = function (options) {
-  this.method = 'POST'
+  this.options = options
   this.region = options.region
-  this.service = 'gamelift'
-  this.canonicalUri = '/'
-  this.date = new Date()
   this.params = options.params || {}
   this.headers = options.headers || {}
   this.payload = options.payload || {}
   this.payload = JSON.stringify(this.payload)
-  this.host = this.service + '.' + this.region + '.amazonaws.com'
+
+  var local = options.local || {}
 
   this.key = GAMELIFT_CREDENTIALS.key
   this.secret = GAMELIFT_CREDENTIALS.secret
 
-  this.headers.Host = this.host // Must be present in headers
+  this.headers.Host = getHost(options.region) // Must be present in headers
   this.headers['X-Amz-Date'] = this.awsDateTimeFormat()
   this.headers['Content-Type'] = 'application/x-amz-json-1.1'
   this.headers['X-Amz-Target'] = 'GameLift.' + options.action
 
-  var reqHeader = JSON.parse(JSON.stringify(this.headers))
-  reqHeader.Authorization = this.getAuthHeader()
+  var requestHeaders = JSON.parse(JSON.stringify(this.headers))
+  requestHeaders.Authorization = this.getAuthHeader()
 
-  var res = Spark.getHttp(this.getRequestUrl())
-    .setHeaders(reqHeader)
+  var requestURL = getRequestURL({
+    region: options.region,
+    canonicalQueryString: this.getCanonicalQueryString(),
+    hasParameters: Object.keys(this.params).length > 0,
+    local: local
+  })
+  var res = Spark.getHttp(requestURL)
+    .setHeaders(requestHeaders)
     .postString(this.payload)
 
   return {
@@ -65,14 +76,50 @@ var AWS = function (options) {
   }
 }
 
-AWS.prototype.getRequestUrl = function () {
-  var u = 'https://' + this.host
+/**
+ * Get the endpoint to issue GameLift requests
+ * @param {Object} options
+ * @param {string} options.region
+ * @param {Object} [options.local]
+ */
+function getEndpoint (options) {
+  if (options.local) {
+    var port = options.local.port || '8080'
 
-  if (Object.keys(this.params).length > 0) {
-    u = +'?' + this.getCanonicalQueryString()
+    return 'http://' + options.local.address + ':' + port
   }
 
-  return u
+  return 'https://' + getHost(options.region)
+}
+
+/**
+ * Get the host of which requests will be performed against.
+ * @param {string} region
+ */
+function getHost (region) {
+  return SERVICE + '.' + region + '.amazonaws.com'
+}
+
+/**
+ * Get the correct request URL depending on the target environment: local or production.
+ * @param {Object} options The parameters of the function
+ * @param {string} options.region The region of the GameLift's fleet
+ * @param {string} options.canonicalQueryString The canonical query string to mount the request URL
+ * @param {boolean} options.hasParameters If parameters are sending alongside the request, needed to prepare the endpoint.
+ * @param {Object} [options.local] - the local object, containing information to make the request ping against GameLiftLocal
+ * @returns {string}
+ */
+function getRequestURL (options) {
+  var endpoint = getEndpoint({
+    local: options.local,
+    region: options.region
+  })
+
+  if (options.hasParameters) {
+    endpoint = +'?' + options.canonicalQueryString
+  }
+
+  return endpoint
 }
 
 AWS.prototype.getCanonicalHeaders = function () {
@@ -119,7 +166,7 @@ AWS.prototype.getAwsCredentialSignature = function () {
 AWS.prototype.getAwsCredentialSignatureKey = function () {
   var h1 = hmac('AWS4' + this.secret, 'TEXT', this.awsDateTimeFormatShort())
   var h2 = hmac(h1, 'HEX', this.region)
-  var h3 = hmac(h2, 'HEX', this.service)
+  var h3 = hmac(h2, 'HEX', SERVICE)
   return hmac(h3, 'HEX', 'aws4_request')
 }
 
@@ -134,8 +181,8 @@ AWS.prototype.getAwsCredentialSignatureValue = function () {
 
 AWS.prototype.getCanonicalRequest = function () {
   return [
-    this.method,
-    this.canonicalUri,
+    DEFAULT_REQUEST_METHOD,
+    '/',
     this.getCanonicalQueryString(),
     this.getCanonicalHeaders(),
     this.getAwsSignedHeaders(),
@@ -156,13 +203,13 @@ AWS.prototype.getAwsCredentialScope = function () {
   return [
     this.awsDateTimeFormatShort(),
     this.region,
-    this.service,
+    SERVICE,
     'aws4_request'
   ].join('/')
 }
 
 AWS.prototype.awsDateTimeFormat = function () {
-  return this.date.toISOString().replace(/[:-]|\.\d{3}/g, '')
+  return new Date().toISOString().replace(/[:-]|\.\d{3}/g, '')
 }
 
 AWS.prototype.awsDateTimeFormatShort = function () {
